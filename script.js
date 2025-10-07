e78e064d70501d72fdcb7bf261a1e7fde2d06a5f
mike15395 <mikhildesai@gmail.com>
2025-10-07 16:28:36 +0530

    added overlay effect+sidebar mobile view fix

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let mobileMenuIcon = document.querySelector(".mobile-menu-icon");
 let mobileSidebar = document.querySelector(".sidebar-mobile-menu");
 let mobileCloseIcon = document.querySelector(".close-icon");
 let companyLogo = document.querySelector(".logo");
+let overlay = document.querySelector(".overlay");
 mobileMenuIcon.addEventListener("click", () => {
   mobileSidebar.style.display = "flex";
   companyLogo.style.display = "none";
+  overlay.style.display = "flex";
 });
 
 mobileCloseIcon.addEventListener("click", () => {
   mobileSidebar.style.display = "none";
   companyLogo.style.display = "flex";
+  overlay.style.display = "none";
 });
